335ec5ef704bf72363c9b5834b50725c621d56ea
Jackie <44011334mm@stu.gdou.edu.cn>
2024-07-29 19:59:05 +0800

    refactor: serve部署环境

--- pensionlens-ui/serve.js
+++ pensionlens-ui/serve.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const os = require('os');
 
 app.use(express.static(path.join(__dirname, './dist')));
 
-// 当请求的文件不存在时，返回index.html文件
+// 当请求的文件不存在时，返回 index.html 文件，以支持 SPA 应用的 history 模式
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
@@ -20,5 +21,21 @@ console.log(`🎉[${new Date()}] PensionLens-ui is successfully running in your 
   '/_/    \\___/_/ /_/____/_/\\____/_/ /_/_____/\\___/_/ /_/____/  \n' +
   '                                                             \n' +
   '  App running at:\n' +
-  '  - Local:   http://localhost:9095/ \n' +
-  '  - Network: http://10.183.158.241:9095/\n')
+  '  - Local:   http://localhost:9095/');
+if(getLocalIPAddresses() !== null){
+  console.log(`  - Network: http://${getLocalIPAddresses()}:9095/\n`)
+}
+
+function getLocalIPAddresses() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4') {
+        if (!iface.internal) {
+          return iface.address;
+        }
+      }
+    }
+  }
+  return null;
+}
